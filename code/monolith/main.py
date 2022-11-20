5aabeee0323a4b00fccc0d168696b471e9318b90
romdalf <romdalf@minime.lan>
2022-11-20 15:32:14 +0100

    adding monolith code

--- code/monolith/main.py
+++ code/monolith/main.py
@@ -13,11 +13,54 @@
 # limitations under the License.
 
 import machine
+from machine import Pin
 import time 
 import network
 import socket
 
-## wireless network connection 
+## motor setup ###########################################################
+
+# defined used pins on the board
+motorOneFW = Pin(18, Pin.OUT)
+motorOneBW = Pin(19, Pin.OUT)
+motorTwoFW = Pin(20, Pin.OUT)
+motorTwoBW = Pin(21, Pin.OUT)
+
+# define movements 
+def moveForward():
+    motorOneFW.value(1)
+    motorTwoFW.value(1)
+    motorOneBW.value(0)
+    motorTwoBW.value(0)
+
+def moveBackward():
+    motorOneFW.value(0)
+    motorTwoFW.value(0)
+    motorOneBW.value(1)
+    motorTwoBW.value(1)
+
+def moveRight():
+    motorOneFW.value(0)
+    motorTwoFW.value(1)
+    motorOneBW.value(1)
+    motorTwoBW.value(0)
+
+def moveLeft():
+    motorOneFW.value(1)
+    motorTwoFW.value(0)
+    motorOneBW.value(0)
+    motorTwoBW.value(1)
+
+def moveStop():
+    motorOneFW.value(0)
+    motorTwoFW.value(0)
+    motorOneBW.value(0)
+    motorTwoBW.value(0)
+
+# default state is fullstop 
+moveStop()
+
+## network connection ####################################################
 
 # SSID with credentials for wireless connection 
 networkName = 'NCC-1031-A'
@@ -33,5 +76,75 @@ def connecting():
         while not wlan.isconnected():
             print('waiting for connection...')
             pass 
-    print('network config: ', wlan.ifconfig())
+    ip = wlan.ifconfig()[0]
+    print(f'Connected on {ip}')
+    return ip 
 
+
+# create a network socket
+def networkSocket(ip):
+    address = (ip, 80)
+    connection = socket.socket()
+    connection.bind(address)
+    connection.listen(1)
+    return connection 
+
+def webControl():
+    html = f"""
+            <!DOCTYPE html>
+            <html>
+            <head>
+            <title>Zumo Robot Control</title>
+            </head>
+            <center><b>
+            <form action="./forward">
+            <input type="submit" value="Forward" style="height:120px; width:120px" />
+            </form>
+            <table><tr>
+            <td><form action="./left">
+            <input type="submit" value="Left" style="height:120px; width:120px" />
+            </form></td>
+            <td><form action="./stop">
+            <input type="submit" value="Stop" style="height:120px; width:120px" />
+            </form></td>
+            <td><form action="./right">
+            <input type="submit" value="Right" style="height:120px; width:120px" />
+            </form></td>
+            </tr></table>
+            <form action="./backward">
+            <input type="submit" value="Back" style="height:120px; width:120px" />
+            </form>
+            </body>
+            </html>
+            """
+    return str(html)
+
+def webServer(connection):
+    while True:
+        client = connection.accept()[0]
+        request = client.recv(1024)
+        request = str(request)
+        try:
+            request = request.split()[1]
+        except IndexError:
+            pass
+        if request == '/forward?':
+            moveForward()
+        elif request == '/backward?':
+            moveBackward()
+        elif request == '/right?':
+            moveRight()
+        elif request == '/left?':
+            moveLeft()
+        elif request == '/stop?':
+            moveStop()
+        html = webControl()
+        client.send(html)
+        client.close()
+
+try:
+    ip = connecting()
+    connection = networkSocket(ip)
+    webServer(connection)
+except KeyboardInterrupt:
+    machine.reset()
